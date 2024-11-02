cae47d0a054e05c6ec0b2d93d565c57f87e1b8eb
Dmitrii <larindmitrii4@gmail.com>
2024-11-02 12:51:33 +0300

    liquidebase add

--- src/main/java/com/example/File/sharing/services/FileService.java
+++ src/main/java/com/example/File/sharing/services/FileService.java
@@ -5,23 +5,22 @@ import com.example.File.sharing.repositories.FileRepository;
 import com.example.File.sharing.exception.FileNotFoundException;
 import com.example.File.sharing.models.File;
 import com.example.File.sharing.repositories.TagRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
-import javax.xml.transform.Source;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
+@Slf4j
 public class FileService {
-    private final long FILE_EXPIRATION_DAYS = 14;
+    static final long FILE_EXPIRATION_DAYS = 14;
 
     private final RestTemplate restTemplate;
 
@@ -74,22 +73,15 @@ public class FileService {
 
 
     public void deleteExpiredFiles() {
-        System.out.println("Deleting expired files");
         LocalDateTime expirationDate = LocalDateTime.now().minusDays(FILE_EXPIRATION_DAYS);
         List<File> expiredFiles = fileRepository.findAllByCreatedAtBefore(expirationDate);
 
         for (File file : expiredFiles) {
             String deleteUrl = "http://localhost:8080/api/v1/files/" + file.getId() + "/delete";
             try {
-                System.out.println(deleteUrl);
-                ResponseEntity<?> response = restTemplate.exchange(deleteUrl, HttpMethod.DELETE, null, Void.class);
-                if (response.getStatusCode().is2xxSuccessful()) {
-                    System.out.println("File with ID " + file.getId() + " deleted successfully.");
-                } else {
-                    System.out.println("Failed to delete file with ID " + file.getId());
-                }
+                restTemplate.exchange(deleteUrl, HttpMethod.DELETE, null, Void.class);
             } catch (Exception e) {
-                System.err.println("Error deleting file with ID " + file.getId() + ": " + e.getMessage());
+                log.error("Error deleting file with ID " + file.getId() + ": " + e.getMessage());
             }
         }
     }
